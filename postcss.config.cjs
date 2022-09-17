19f7d2c6f0cf89825a6a7f37d938e28940875ab2
Themanuca <ssergioca@gmail.com>
2022-09-17 12:56:18 -0300

    feat: tailwind and assets

---
+++ postcss.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+const k = import("tailwindcss").Config;
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+// module.exports = {
+//   plugins: [require("tailwindcss"), require("autoprefixer")],
+// };
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import "./styles/main.css";
 function App() {
-  return <h1>Olá Mundo</h1>;
+  return <div className="w-80 h-80 bg-black dark:bg-violet-500">Olá Mundo</div>;
 }
 
 export default App;
