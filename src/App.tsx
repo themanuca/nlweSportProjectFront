786cce32c55a022526f7b72c0a77ed7dfba55fda
Themanuca <ssergioca@gmail.com>
2022-09-22 01:02:34 -0300

    feat: img e estilização

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,81 @@ function App() {
     <div className="max-w-[1344] mx-auto flex flex-col items-center my-20">
       <img src={logoimg} />
 
-      <h1 className="text-6xl text-white font-black m-20">Seu Duo está aqui</h1>
+      <h1 className="text-6xl text-white font-black m-20">
+        Seu
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          Duo
+        </span>
+        está aqui
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="./src/assets/game1.png"></img>
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+
+            <span className="text-zinc-300 text-sm block ">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="./src/assets/game2.png"></img>
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Dota 2</strong>
+
+            <span className="text-zinc-300 text-sm block ">6 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="./src/assets/game3.png"></img>
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              Counter Strike
+            </strong>
+
+            <span className="text-zinc-300 text-sm block ">20 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="./src/assets/game5.png"></img>
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex Legends</strong>
+
+            <span className="text-zinc-300 text-sm block ">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="./src/assets/game6.png"></img>
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Fortnite</strong>
+
+            <span className="text-zinc-300 text-sm block ">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="./src/assets/game7.png"></img>
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              World of Warcraft
+            </strong>
+
+            <span className="text-zinc-300 text-sm block ">4 anúncios</span>
+          </div>
+        </a>
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py6 rounded-b-lg"></div>
+      </div>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,11 @@ module.exports = {
     extend: {
       backgroundImage: {
         fundo: "url('/fundo.png')",
+        "nlw-gradient":
+          "linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 50.94%, #E1D55D 40.57%)",
+
+        "game-gradient":
+          "linear-gradient(180deg, rgb(0,0,0,0) 0%, rgb(0,0,0,0.9) 67.08%)",
       },
     },
   },
